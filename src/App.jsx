ef6d4f9da16ccdce7af8b14a354a58514a878160
nugurejeil <nugurejeil1@gmail.com>
2025-07-25 16:16:16 +0900

    배포 연습

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+    return (
+        <>
+            <h1>This is Deploy TEST!!!</h1>
+            <h2>나는 이제 배포의 달인</h2>
+            Hello Vite-React!
+        </>
+    )
+}
+
+export default App
